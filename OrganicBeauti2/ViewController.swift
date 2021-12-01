7aedb924507e309c5b2313c8435d0862703048c5
fdocharles <fdocharles@gmail.com>
2021-12-01 11:54:56 -0500

    Initial Commit

---
+++ OrganicBeauti2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrganicBeauti2
+//
+//  Created by user199800 on 12/1/21.
+//  Copyright © 2021 user199800. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
