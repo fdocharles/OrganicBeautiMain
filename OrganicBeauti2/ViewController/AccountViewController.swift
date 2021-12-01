97fd5b401a50e6830355196d734dc7262f678a89
fdocharles <fdocharles@gmail.com>
2021-12-01 13:07:44 -0500

    Initial PROJECT

---
+++ OrganicBeauti2/ViewController/AccountViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AccountViewController.swift
+//  OrganicBeauti2
+//
+//  Created by user199800 on 12/1/21.
+//  Copyright © 2021 user199800. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
+
+
