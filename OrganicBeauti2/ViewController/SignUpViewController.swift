8918f8fa3c6d8b16f47b0272e129e5c00c75234b
fdocharles <fdocharles@gmail.com>
2021-12-02 21:16:29 -0500

    Completed UI for Signup view

---
+++ OrganicBeauti2/ViewController/SignUpViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpViewController.swift
+//  OrganicBeauti2
+//
+//  Created by user199800 on 12/2/21.
+//  Copyright © 2021 user199800. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var signupButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Custom UI styles
+        //Signup button
+        signupButton.layer.cornerRadius = 25.0
+    }
+    
+}
